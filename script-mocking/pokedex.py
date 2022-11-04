b4973a2ab02dd7569157e67b90695feaef3f75da
Justin <justinbaur@outlook.com>
2022-11-03 20:09:21 -0500

    add mocks to script test

--- script-mocking/pokedex.py
+++ script-mocking/pokedex.py
@@ -3,6 +3,9 @@ from rich import print
 from typing import Any
 
 def get_pokedex_entry(name: str) -> dict[str, Any]:
+    """
+    Return a Pokedex entry from the public API based on the Pokemon's common name
+    """
     pokedex_entry = {}
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
     if response.status_code == 200:
@@ -12,6 +15,9 @@ def get_pokedex_entry(name: str) -> dict[str, Any]:
     return pokedex_entry
 
 def display_pokedex_entry(name: str) -> None:
+    """
+    Format and display a Pokedex entry 
+    """
     pokedex_entry = get_pokedex_entry(name)
     pokemon_name = pokedex_entry.get("name", "unknown")
     pokedex_number = pokedex_entry.get("id", -1)
@@ -19,4 +25,6 @@ def display_pokedex_entry(name: str) -> None:
     print(f"[blue]Pokedex Entry {pokedex_number}[/blue]")
     print(f"[blue]{pokemon_name}[/blue]")
 
-display_pokedex_entry("weedle")
+
+if __name__ == "__main__":
+    display_pokedex_entry("snorlax")
--- script-mocking/test_pokedex.py
+++ script-mocking/test_pokedex.py
@@ -1,6 +1,23 @@
 from pokedex import get_pokedex_entry
 
-def test_get_ok():
+class MockResponse:
+
+    def __init__(self, status_code: int, json_data: dict = None, text: str = None) -> None:
+        self.status_code = status_code
+        self.json_data = json_data
+        self.text = text
+
+    def json(self) -> dict:
+        return self.json_data
+
+
+def test_get_ok(mocker) -> None:
+    input = "weedle"
     expected_response = {"name": "weedle", "id": 13}
-    actual_response = get_pokedex_entry("weedle")
-    assert actual_response == expected_response
+
+    mocker.patch("pokedex.requests.get", return_value=MockResponse(200, expected_response))
+    
+    actual_response = get_pokedex_entry(input)
+
+    assert actual_response.get("name") == input, 'incorrect pokedex name'
+    assert actual_response == expected_response, 'incorrect pokedex entry'
